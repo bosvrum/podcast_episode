c024ae49d7c60fccc0f5372f2c8e0a9e6485fe12
Catalin <catalinboruga@yahoo.com>
2015-05-25 14:23:06 +0200

    Add paperclip image

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,5 @@
 class Episode < ActiveRecord::Base
+  has_attached_file :episode_thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#"}
+  validates_attachment_content_type :episode_thumbnail, :content_type => /\Aimage\/.*\Z/  
   belongs_to :podcast
 end
